77c5574f358d397fdc046a475093eb6088b94e05
markogrady1 <markogrady.code@gmail.com>
2016-03-25 19:46:54 +0000

    add pages and features to create a working draft

--- assets/js/sortTable.js
+++ assets/js/sortTable.js
@@ -12,8 +12,68 @@
  * when a given header is clicked
  *
  */
-var sortTable = function() {
+var sortHottestReposTable = function() {
     var table = document.getElementById('xtable')
+        ,tableHead = table.querySelector('thead')
+        ,tableHeaders = tableHead.querySelectorAll('th')
+        ,tableBody = table.querySelector('tbody')
+        ;
+    tableHead.addEventListener('click',function(e){
+        var tableHeader = e.target
+            ,textContent = tableHeader.textContent
+            ,tableHeaderIndex,isAscending,order
+            ;
+        if (textContent!=='add row') {
+            while (tableHeader.nodeName!=='TH') {
+                tableHeader = tableHeader.parentNode;
+            }
+            tableHeaderIndex = Array.prototype.indexOf.call(tableHeaders,tableHeader);
+            isAscending = tableHeader.getAttribute('data-order')==='asc';
+            order = isAscending?'desc':'asc';
+            tableHeader.setAttribute('data-order',order);
+            tinysort(
+                tableBody.querySelectorAll('tr')
+                ,{
+                    selector:'td:nth-child('+(tableHeaderIndex+1)+')'
+                    ,order: order
+                }
+            );
+        }
+    });
+};
+
+var sortProgrammingLanguageTable = function() {
+    var table = document.getElementById('ytable')
+        ,tableHead = table.querySelector('thead')
+        ,tableHeaders = tableHead.querySelectorAll('th')
+        ,tableBody = table.querySelector('tbody')
+        ;
+    tableHead.addEventListener('click',function(e){
+        var tableHeader = e.target
+            ,textContent = tableHeader.textContent
+            ,tableHeaderIndex,isAscending,order
+            ;
+        if (textContent!=='add row') {
+            while (tableHeader.nodeName!=='TH') {
+                tableHeader = tableHeader.parentNode;
+            }
+            tableHeaderIndex = Array.prototype.indexOf.call(tableHeaders,tableHeader);
+            isAscending = tableHeader.getAttribute('data-order')==='asc';
+            order = isAscending?'desc':'asc';
+            tableHeader.setAttribute('data-order',order);
+            tinysort(
+                tableBody.querySelectorAll('tr')
+                ,{
+                    selector:'td:nth-child('+(tableHeaderIndex+1)+')'
+                    ,order: order
+                }
+            );
+        }
+    });
+};
+
+var sortWebDevTable = function() {
+    var table = document.getElementById('ztable')
         ,tableHead = table.querySelector('thead')
         ,tableHeaders = tableHead.querySelectorAll('th')
         ,tableBody = table.querySelector('tbody')
