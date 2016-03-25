9254b87a4d19df86ca8fc04d139a90b6ce786d30
markogrady1 <markogrady.code@gmail.com>
2016-03-25 20:06:37 +0000

    add new pages

---
+++ assets/js/tableValues.js
@@ -0,0 +1,119 @@
+var hottestRepoArray = [
+    {
+        name: "Bootstrap",
+        url: "https://github.com/twbs/bootstrap"
+    },{
+        name: "Node.js",
+        url: "https://github.com/joyent/node"
+    },{
+        name: "reactJS",
+        url: "https://github.com/facebook/react"
+    },{
+        name: "JQuery",
+        url: "https://github.com/jquery/jquery"
+    },{
+        name: "gitignore",
+        url: "https://github.com/github/gitignore"
+    },{
+        name: "Ruby on Rails",
+        url: "https://github.com/rails/rails"
+    },{
+        name: "D3.js",
+        url: "	https://github.com/mbostock/d3"
+    },{
+        name: "Impress.js",
+        url: "https://github.com/bartaz/impress.js"
+    },{
+        name: "Font Awesome",
+        url: "https://github.com/FortAwesome/Font-Awesome"
+    },{
+        name: "Backbone.js",
+        url: "https://github.com/jashkenas/backbone"
+    },{
+        name: "Homebrew",
+        url: "https://github.com/mxcl/homebrew"
+    },{
+        name: "animate.css",
+        url: "https://github.com/daneden/animate.css"
+    },{
+        name: "normalize.css",
+        url: "https://github.com/necolas/normalize.css"
+    },{
+        name: "Modernizr",
+        url: "https://github.com/Modernizr/Modernizr"
+    },{
+        name: "960-Grid-System",
+        url: "https://github.com/nathansmith/960-Grid-System"
+    }
+];
+
+
+
+var programmingLanguagesArray = [
+    {
+        name: "Swift",
+        url: "https://github.com/apple/swift"
+    },{
+        name: "Rust",
+        url: "https://github.com/rust-lang/rust"
+    },{
+        name: "Go",
+        url: "https://github.com/golang/go"
+    },{
+        name: "CoffeeScript",
+        url: "https://github.com/jashkenas/coffeescript"
+    },{
+        name: "Ruby",
+        url: "https://github.com/ruby/ruby"
+    },{
+        name: "PHP-src",
+        url: "https://github.com/php/php-src"
+    },{
+        name: "Clojure",
+        url: "https://github.com/clojure/clojure"
+    },{
+        name: "Scala",
+        url: "https://github.com/scala/scala"
+    }
+];
+
+
+var webDevArray = [
+    {
+        name: "Meteor",
+        url: "https://github.com/meteor/meteor"
+    },{
+        name: "Express.js",
+        url: "https://github.com/expressjs/express"
+    },{
+        name: "Laravel",
+        url: "https://github.com/laravel/laravel"
+    },{
+        name: "Flask",
+        url: "https://github.com/mitsuhiko/flask"
+    },{
+        name: "Django",
+        url: "https://github.com/django/django"
+    },{
+        name: "Symfony",
+        url: "https://github.com/symfony/symfony"
+    },{
+        name: "Sinatra",
+        url: "https://github.com/sinatra/sinatra"
+    },{
+        name: "CakePHP",
+        url: "https://github.com/cakephp/cakephp"
+    },{
+        name: "Mojo",
+        url: "https://github.com/django/django"
+    },{
+        name: "Sails",
+        url: "https://github.com/balderdashy/sails"
+    },{
+        name: "MEAN",
+        url: "https://github.com/linnovate/mean"
+    },{
+        name: "Derby",
+        url: "https://github.com/derbyjs/derby"
+    }
+];
